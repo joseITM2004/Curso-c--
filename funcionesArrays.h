9fde3afee89ae7d59db6a41dc3ebf209a74e0a37
Jose David <josegaviria321793@correo.itm.edu.co>
2025-09-02 14:49:15 -0500

    ActualizacionLibrería

--- funcionesArrays.h
+++ funcionesArrays.h
@@ -1,11 +1,15 @@
 #pragma once
-#include<iostream>
+#include <iostream>
 using namespace std;
-void ordenarArreglo(int arr[],int size){
-    for (int i = 0; i < size; i++) {
-        for (int j = i + 1; j < size; j++) {
+void ordenarArreglo(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
             int aux = 0;
-            if (arr[i] > arr[j]) {
+            if (arr[i] > arr[j])
+            {
                 aux = arr[i];
                 arr[i] = arr[j];
                 arr[j] = aux;
@@ -14,18 +18,46 @@ void ordenarArreglo(int arr[],int size){
     }
 }
 
-void imprimirArray(int arr[],int size){
+void imprimirArray(int arr[], int size)
+{
     for (int i = 0; i < size; i++)
     {
-        cout<<arr[i] << "|" ;
+        cout << arr[i] << "|";
     }
-    cout<<endl;
+    cout << endl;
 }
-void llenarArray(int arr[]){
-    int size=sizeof(arr)/sizeof(arr[0]);
-    for (int i = 0; i <size ; i++)
+void llenarArray(int arr[])
+{
+    int size = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < size; i++)
     {
-        cout<<"ingrese el dato"<<i+1<<endl;
-        cin>>arr[i];
+        cout << "ingrese el dato" << i + 1 << endl;
+        cin >> arr[i];
     }
+}
+int menorArray(int arr[], int size)
+{
+
+    int menor = arr[0];
+    for (int i = 0; i < size; i++)
+    {
+
+        if (arr[i] < menor)
+        {
+
+            menor = arr[i];
+        }
+    }
+    return menor;
+}
+int promedioArray(int arr[], int size)
+{
+    int average=0;
+    for (int i = 0; i < size; i++)
+    {
+
+        average += arr[i];
+    }
+
+    average /= size;
 }
